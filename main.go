76eaafe661b56a88eb03cd4b86aa696b12a46c02
BuildTools <unconfigured@null.spigotmc.org>
2024-01-17 22:04:00 +0100

    implementet /lehrerranking/lehrer

--- main.go
+++ main.go
@@ -1,12 +1,61 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
+
+func getTeachers() []string {
+	// Fetch HTML content
+	resp, err := http.Get("https://www.nepomucenum.de/wir-am-nepo/lehrende/")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	// Read HTML content
+	html, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	// Define the regex pattern
+	pattern := `<h3[^>]\s*class\s*=\s*["']\s*team-member-name\s*["'][^>]*>(.*?)<\/h3>`
+	re := regexp.MustCompile(pattern)
+
+	// Find matches in HTML
+	matches := re.FindAllStringSubmatch(string(html), -1)
+
+	// Extract and print matched strings
+	ret := []string{}
+	for _, match := range matches {
+		ret = append(ret, match[1])
+	}
+	return ret
+}
+
+var teachers = getTeachers()
+
+func Teachers(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, strings.Join(teachers, ", "))
+}
+
+func getJson() map[string]map[string]int {
+	data := map[string]map[string]int{}
+	fileContent, _ := os.ReadFile("results1.json")
+	json.Unmarshal(fileContent, &data)
+	return data
+}
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome!\n")
@@ -18,9 +67,24 @@ func TeacherRatingUpload(w http.ResponseWriter, r *http.Request, ps httprouter.P
 }
 
 func main() {
-	router := httprouter.New()
-	router.GET("/", Index)
-	router.POST("/", TeacherRatingUpload)
+	results1 := getJson()
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	for category, ratings := range results1 {
+		print(category + ":\t")
+		for _, t := range teachers {
+			print(strconv.Itoa(ratings[t]) + "\t")
+		}
+		println()
+	}
+	println()
+
+	getTeachers()
+
+	router := httprouter.New()
+	router.POST("/lehrerranking", TeacherRatingUpload)
+	router.GET("/lehrerranking/validate", Index)
+	router.GET("/lehrerranking/results", Index)
+	router.GET("/lehrerranking/lehrer", Teachers)
+
+	log.Fatal(http.ListenAndServe(":1337", router))
 }
